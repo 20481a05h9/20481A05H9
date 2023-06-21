d4b2cdbd51505ec7c329e2146fa9064d016b871c
20481a05h9 <99808125+20481a05h9@users.noreply.github.com>
2023-06-21 14:01:30 +0530

    Add files via upload

---
+++ Number/Components/Numbers.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+function Numbers() {
+  // Render the numbers received from the server
+  return (
+    <div>
+      <h2>Numbers</h2>
+      { <p>[ 1, 2, 3, 5, 8, 13 ]</p>}
+    </div>
+     
+  );
+}
+
+export default Numbers;
